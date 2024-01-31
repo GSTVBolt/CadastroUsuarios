4325034ea3bf48b4b446b82f4d5f0b68229a5384
Gustavo <gustavo_monteiro.20@hotmail.com>
2024-01-31 11:33:14 -0300

    Implementando funcionalidades aos componentes

--- crud/frontend/src/components/template/Logo.jsx
+++ crud/frontend/src/components/template/Logo.jsx
@@ -1,9 +1,13 @@
 import './Logo.css'
+import logo from '../../assets/imgs/logo.png'
 import React from 'react'
+// import { Link } from 'react-router-dom'
 
 const Logo = props =>
-    <aside>
-        Logo
+    <aside className='logo'>
+        <a href='/' className='logo'>
+          <img src={logo} alt='logo'/>  
+        </a>
     </aside>
 
 export default Logo
--- crud/frontend/src/components/template/Nav.jsx
+++ crud/frontend/src/components/template/Nav.jsx
@@ -1,9 +1,13 @@
 import './Nav.css'
 import React from 'react'
+import NavItem from '../NavItem'
 
 const Nav = props =>
     <aside className="menu-area">
-        Navegação
+        <nav className="menu">
+            <NavItem href="#/" icon="home" title="Início" />
+            <NavItem href="#/Users" icon="users" title="Usuário" />
+        </nav>
     </aside>
 
 export default Nav
